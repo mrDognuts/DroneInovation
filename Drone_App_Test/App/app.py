b1fba69215563e7db9f2c419eed4b3d921cf6f21
Walter Nienaber <112818645+mrDognuts@users.noreply.github.com>
2024-11-12 18:44:19 +0200

    up

--- Drone_App_Test/App/app.py
+++ Drone_App_Test/App/app.py
@@ -16,31 +16,20 @@ app = Flask(__name__)
 # Load the model once globally
 def load_model(weights_path, device="cpu"):
     cfg = get_cfg()
-    # Use the Mask R-CNN model with ResNet50 and FPN backbone, 3x schedule
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # Set to 1 if it's just "damage" as the single class
-    cfg.MODEL.DEVICE = device  # Set to "cpu" to use CPU or "cuda" for GPU
-    cfg.MODEL.WEIGHTS = weights_path  # Path to your trained model weights
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.6  # Initial prediction threshold (adjustable in code)
-    cfg.INPUT.MIN_SIZE_TEST = 800
-    cfg.INPUT.MAX_SIZE_TEST = 1333
-
-    # Set metadata for visualization
-    MetadataCatalog.get("car_dataset").set(thing_classes=["damage"])
     
+    cfg.MODEL.WEIGHTS = weights_path  # Path to your trained model weights
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # Single class for damage detection
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.3  # Confidence threshold for inference
+    
+    # Set device for inference (CPU or CUDA)
+    cfg.MODEL.DEVICE = device 
+    
+    # Initialize the predictor
     return DefaultPredictor(cfg)
 
-# Function to apply custom Non-Maximum Suppression (NMS)
-def custom_nms(instances, nms_thresh=0.5):
-    boxes = instances.pred_boxes.tensor
-    scores = instances.scores
-    keep = torch.ops.torchvision.nms(boxes, scores, nms_thresh)
-    return instances[keep]
-
-# Path to your trained model weights (make sure this path is correct)
-weights_path = "C:/Users/walte/Downloads/0Github/DroneBackup/DroneInovation/Drone/model_final.pth"
-
-# Initialize the predictor with the path to weights
+# Path 
+weights_path = r"C:\Users\walte\Downloads\0Github\PRJ_FINAL\DroneInovation\Drone_App_Test\App\model_final.pth"
 predictor = load_model(weights_path)
 
 # Define route for the home page
@@ -73,12 +62,8 @@ def predict():
     filtered_instances = instances[(instances.scores > min_confidence) & 
                                    (instances.pred_boxes.area() > min_area)]
     
-    # Apply Non-Maximum Suppression to filtered instances
-    nms_thresh = 0.5  # Adjust NMS threshold if needed
-    filtered_instances = custom_nms(filtered_instances, nms_thresh)
-    
     # Visualize predictions
-    v = Visualizer(img[:, :, ::-1], MetadataCatalog.get("car_dataset"), scale=0.8)
+    v = Visualizer(img[:, :, ::-1], MetadataCatalog.get("car_dataset_train"), scale=0.8)
     v = v.draw_instance_predictions(filtered_instances)
     processed_image = v.get_image()[:, :, ::-1]
     
